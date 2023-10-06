a28dc2ddf1095fd6a0ab20ef2a5160f58790362d
Ujjwal Jaguri <ujjwal.jaguri.19012003@gmail.com>
2023-10-06 20:37:45 +0530

    initial:init (functionality not added yet)

---
+++ index.js
@@ -0,0 +1,21 @@
+const getCountries = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
+  data.forEach((item) => {
+    console.log(item);
+    document.getElementById("countries").innerHTML += `
+        <div class = "country">
+            <img class = "imgOfCountry" src=${item.flag} alt="img" />
+            <div class="country-info">
+                <h3 class = "nameOfCountry">   ${item.name}</h3>
+                <p>     Population: ${item.population}</p>
+                <p>     Region: ${item.region}</p>
+                <p>     Capital: ${item.capital}</p>
+            </div>
+        </div>
+    `;
+  });
+};
+
+getCountries("/data.json");
